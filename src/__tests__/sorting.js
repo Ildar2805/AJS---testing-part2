153c67b486e1e9861e84404ea92aeae78c6b38cb
Ildar <ildar2805@gmail.com>
2023-02-06 18:35:14 +0300

    V1

---
+++ src/__tests__/sorting.js
@@ -0,0 +1,18 @@
+import sorting from '../sorting';
+
+const heroes = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+const expected = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+test('testing function sorting', () => {
+  const result = sorting(heroes);
+  expect(result).toEqual(expected);
+});
